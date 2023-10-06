5e2428b10d9fd42a106756ad2b83fee4f7337f7f
Ahmet Yusuf Yılmaz <ahmetyilmaz@atlasyazilim.com.tr>
2023-10-06 16:04:02 +0300

    Distributed Caching

--- Distributed.Caching/Program.cs
+++ Distributed.Caching/Program.cs
@@ -1,15 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers(); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddStackExchangeRedisCache(options=>options.Configuration="localhost:1453"); // eklendi.
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
